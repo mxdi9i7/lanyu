163979db42bb526a6310f97404cf7e4fa6eccd8d
mxdi9i7 <peter.zheng88228@gmail.com>
2016-11-05 14:11:37 -0400

    asdf

--- visit.php
+++ visit.php
@@ -4,19 +4,18 @@
 <title>Lanyu | Showroom</title>
 
 <section class="visit-section">
-	<img src="http://i.imgur.com/DfwzM14.jpg" class="responsive-img">
 	<div class="location-group row container">
 		<div class="col s6 m3 location-card">
 		  <div class="card small">
 		    <div class="card-image waves-effect waves-block waves-light">
-		      <img class="activator responsive-img" src="http://www.phoenixtcm.org.uk/wp-content/uploads/2015/10/Shanghai-Skyline.jpg">
+		      <img class="activator responsive-img" src="http://i.imgur.com/u9YT57o.jpg">
 		    </div>
 		    <div class="card-content">
-		      <span class="card-title activator grey-text text-darken-4">Shanghai</span>
+		      <span class="card-title activator grey-text text-darken-4">New York</span>
 		      <p><a href="#" class="activator">Click for more info</a></p>
 		    </div>
 		    <div class="card-reveal">
-		      <span class="card-title grey-text text-darken-4">Shanghai<i class="material-icons right">close</i></span>
+		      <span class="card-title grey-text text-darken-4">New York<i class="material-icons right">close</i></span>
 		      <p class="center">16 West 23rd Street, 4F <br> New York<br> NY 10010</p>
 		      <p class="center">Email: info@lanyu.com</p>
 		    </div>
@@ -33,38 +32,38 @@
 		    </div>
 		    <div class="card-reveal">
 		      <span class="card-title grey-text text-darken-4">Shanghai<i class="material-icons right">close</i></span>
-		      <p class="center">16 West 23rd Street, 4F <br> New York<br> NY 10010</p>
-		      <p class="center">Email: info@lanyu.com</p>
+		      <p class="center">No. 29 <br> Purple Bamboo Road, Shangri-La Hotel on the 12th floor <br> Haidian District, Beijing</p>
+		      <p class="center">+86 10-68465922</p>
 		    </div>
 		  </div>
 		</div>
 		<div class="col s6 m3 location-card">
 		  <div class="card small">
 		    <div class="card-image waves-effect waves-block waves-light">
-		      <img class="activator responsive-img" src="http://www.phoenixtcm.org.uk/wp-content/uploads/2015/10/Shanghai-Skyline.jpg">
+		      <img class="activator responsive-img" src="http://i.imgur.com/aVSrDPE.jpg">
 		    </div>
 		    <div class="card-content">
-		      <span class="card-title activator grey-text text-darken-4">Shanghai</span>
+		      <span class="card-title activator grey-text text-darken-4">Beijing</span>
 		      <p><a href="#" class="activator">Click for more info</a></p>
 		    </div>
 		    <div class="card-reveal">
-		      <span class="card-title grey-text text-darken-4">Shanghai<i class="material-icons right">close</i></span>
-		      <p class="center">16 West 23rd Street, 4F <br> New York<br> NY 10010</p>
-		      <p class="center">Email: info@lanyu.com</p>
+		      <span class="card-title grey-text text-darken-4">Beijing<i class="material-icons right">close</i></span>
+		      <p class="center">No. 99 <br> Beijing East Road ,The Bund Yifeng source 211A <br> Huangpu District , Shanghai</p>
+		      <p class="center">+86 21-63338550</p>
 		    </div>
 		  </div>
 		</div>
 		<div class="col s6 m3 location-card">
 		  <div class="card small">
 		    <div class="card-image waves-effect waves-block waves-light">
-		      <img class="activator responsive-img" src="http://www.phoenixtcm.org.uk/wp-content/uploads/2015/10/Shanghai-Skyline.jpg">
+		      <img class="activator responsive-img" src="http://i.imgur.com/qxzS0DQ.jpg">
 		    </div>
 		    <div class="card-content">
-		      <span class="card-title activator grey-text text-darken-4">Shanghai</span>
+		      <span class="card-title activator grey-text text-darken-4">Paris</span>
 		      <p><a href="#" class="activator">Click for more info</a></p>
 		    </div>
 		    <div class="card-reveal">
-		      <span class="card-title grey-text text-darken-4">Shanghai<i class="material-icons right">close</i></span>
+		      <span class="card-title grey-text text-darken-4">Paris<i class="material-icons right">close</i></span>
 		      <p class="center">16 West 23rd Street, 4F <br> New York<br> NY 10010</p>
 		      <p class="center">Email: info@lanyu.com</p>
 		    </div>
