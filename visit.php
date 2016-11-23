3a2f223b2cd957bc8672b4983c182ee16471d339
mxdi9i7 <peter.zheng88228@gmail.com>
2016-11-22 19:44:53 -0500

    fixes on about page

--- visit.php
+++ visit.php
@@ -17,6 +17,7 @@
 					<p>16 West 23rd Street, 4F</p>
 					<p>New York</p>
 					<p>NY 10010</p>
+					<a href="https://www.google.com/maps/place/16+W+23rd+St,+New+York,+NY+10010/@40.741631,-73.992624,17z/data=!3m1!4b1!4m5!3m4!1s0x89c259a474a033bd:0x986d64f10ba9db32!8m2!3d40.741631!4d-73.9904353">Find us on Google Maps</a>
 				</div>
 				<div class="col s6">
 					<h4>Contact</h4>
@@ -39,6 +40,7 @@
 					</p>
 					<p>Huangpu District</p>
 					<p>Shanghai</p>
+					<a href="https://www.google.com/maps/place/LANYU/@31.2408634,121.4180847,12z/data=!4m8!1m2!2m1!1z5YWw546JIOS4iua1tw!3m4!1s0x35b27057043e7b87:0x38a0f536acb5b98d!8m2!3d31.2407172!4d121.4880366">Find us on Google Maps</a>
 				</div>
 				<div class="col s6">
 					<h4>Contact</h4>
@@ -59,6 +61,7 @@
 					<p>No. 29 Purple Bamboo Road, Shangri-La Hotel on the 12th floor</p> 
 					<p> Haidian District</p>
 					<p>Beijing</p>
+					<a href="https://www.google.com/maps/place/%E5%85%B0%E7%8E%89%E9%AB%98%E7%BA%A7%E5%AE%9A%E5%88%B6/@39.9443113,116.3044554,17.88z/data=!4m5!3m4!1s0x35f05199a862e151:0xb02915e4cddf3921!8m2!3d39.9442175!4d116.3049639">Find us on Google Maps</a>
 				</div>
 				<div class="col s6">
 					<h4>Contact</h4>
@@ -67,21 +70,23 @@
 				</div>
 			</div>
 		</div>
-		<div class="location-container">
-		<img class="location-img" src="http://i.imgur.com/BXTNCFn.jpg">
+	</div>
+
+	<div class="location-container">
+		<img class="location-img responsive-img" src="http://i.imgur.com/BXTNCFn.jpg">
 		<div class="location-overlay">
 			<h1>Los Angeles Showroom</h1>
 			<div class="row">
 				<div class="col s6">
 					<h4>Location</h4>
-					<p>600 N Rosemead Blvd. Suite 204 </p>
+					<p>600 North Rosemead Blvd. Suite 204 </p>
 					<p>Pasadena</p>
 					<p>CA 91107</p>
+					<a href="https://www.google.com/maps/place/600+N+Rosemead+Blvd,+Pasadena,+CA+91107/@34.1567466,-118.0816799,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2dc9b6c91df57:0x8a2c5d6dbe0a5fc8!8m2!3d34.1567422!4d-118.0794912">Find us on Google Maps</a>
 				</div>
 				<div class="col s6">
 					<h4>Contact</h4>
 					<p>Email: <a href="mailto:info@lanyu.com">info@lanyu.com</a></p>
-					<p>Phone: +86 10-68465922</p>
 				</div>
 			</div>
 		</div>
