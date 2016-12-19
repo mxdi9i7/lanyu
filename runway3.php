89446c63037d7812c72463acc026411ff8ca61c0
mxdi9i7 <peter.zheng88228@gmail.com>
2016-12-18 19:37:36 -0500

    bridals -> bridal

--- runway3.php
+++ runway3.php
@@ -9,7 +9,7 @@
     include ('inc/preloader.php');
   ?>
 <section class="runway-section">
-    <h1>Bridals S/S 2017 <br>Zodiac Constellations @ New York Fashion Week</h1>
+    <h1>Bridal S/S 2017 <br>Zodiac Constellations @ New York Fashion Week</h1>
     <div class="runway-buttons">
         <a href="runway2.php" class="btn btn-large waves-effect waves-light white black-text">Previous</a>
         <a href="runway4.php" class="btn btn-large waves-effect waves-light white black-text right">Next</a>
